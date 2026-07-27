7c15d38643a46fbe148c03f82b2043d0cf73f60b
Kevin Hofer <kevin.hofer@widas.de>
2026-07-27 08:53:34 +0200

    chore(): restructure

--- service/handler/scan_handler.py
+++ service/handler/scan_handler.py
@@ -1,16 +1,16 @@
 from fastapi import Request
 import numpy as np
 
-from service import debug_storage
+from service.utils import volume_writer
 
 
 def handle_frame(request: Request, img: np.ndarray):
     warped, sorted_pts = request.app.state.segmentor.segment_and_warp(img)
-    trace_id = debug_storage.new_trace_id()
+    trace_id = volume_writer.new_trace_id()
 
     if request.app.state.debug:
-        debug_storage.save_frame(trace_id, "00_raw", img)
-        debug_storage.save_json(trace_id, "01_sorted_pts", {
+        volume_writer.save_frame(trace_id, "00_raw", img)
+        volume_writer.save_json(trace_id, "01_sorted_pts", {
             "pts": sorted_pts.tolist() if sorted_pts is not None else None
         }) 
  
@@ -24,11 +24,11 @@ def handle_frame(request: Request, img: np.ndarray):
     edition_dets, ed_crops = request.app.state.ed_check.predict(frame=warped)
 
     if request.app.state.debug:
-        debug_storage.save_frame(trace_id, "02_warped", warped)
-        debug_storage.save_crops(trace_id, "03_ocr_crop", ocr_crops)
-        debug_storage.save_json(trace_id, "03_ocr_result", {"text": text})
-        debug_storage.save_crops(trace_id, "04_edition_crop", ed_crops)
-        debug_storage.save_json(trace_id, "04_edition_result", {"editions": edition_dets})
+        volume_writer.save_frame(trace_id, "02_warped", warped)
+        volume_writer.save_crops(trace_id, "03_ocr_crop", ocr_crops)
+        volume_writer.save_json(trace_id, "03_ocr_result", {"text": text})
+        volume_writer.save_crops(trace_id, "04_edition_crop", ed_crops)
+        volume_writer.save_json(trace_id, "04_edition_result", {"editions": edition_dets})
     
     return {
         "text": text,
